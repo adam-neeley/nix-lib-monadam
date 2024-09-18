a556bde63e3792e583e45aa9b5281b8aed1f417c
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-18 10:01:14 -0700

    move lib to flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,27 @@
   description = "monadam's Nix library";
 
   outputs = { self, nixpkgs }:
-    let pkgs = import nixpkgs { };
-    in { lib = import ./lib { inherit (pkgs) lib; }; };
+    let
+      pkgs = import nixpkgs { };
+      inherit (builtins)
+        attrValues readDir pathExists concatLists concatStringsSep;
+      inherit (pkgs.lib)
+        collect isString mapAttrs filter id mapAttrsRecursive mapAttrsToList
+        filterAttrs hasPrefix hasSuffix nameValuePair removeSuffix;
+    in {
+      lib = rec {
+        getDir = dir:
+          mapAttrs (file: type:
+            if (type == "directory") then getDir "${dir}/${file}" else type)
+          (builtins.readDir dir);
+        getDirs = dir: pkgs.lib.attrsets.mapAttrsToList (k: v: k) (getDir dir);
+        files = dir:
+          collect isString
+          (mapAttrsRecursive (path: type: concatStringsSep "/" path)
+            (getDir dir));
+        packageFiles = dir:
+          map (file: ./. + "/${file}")
+          (filter (file: (baseNameOf file) == "package.nix") (files dir));
+      };
+    };
 }
