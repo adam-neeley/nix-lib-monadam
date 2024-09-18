75039140839fff256b949da08e6295693b8ffa9b
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-18 12:41:32 -0700

    update lib

--- flake.nix
+++ flake.nix
@@ -8,19 +8,26 @@
       inherit (builtins)
         attrValues readDir pathExists concatLists concatStringsSep;
       inherit (pkgs.lib)
-        collect isString mapAttrs filter id mapAttrsRecursive mapAttrsToList
-        filterAttrs hasPrefix hasSuffix nameValuePair removeSuffix;
+        strings collect isString mapAttrs filter id mapAttrsRecursive
+        mapAttrsToList filterAttrs hasPrefix hasSuffix nameValuePair
+        removeSuffix;
     in {
       lib = rec {
-        getDir = dir:
+        readDirRec = dir:
           mapAttrs (file: type:
-            if (type == "directory") then getDir "${dir}/${file}" else type)
+            if (type == "directory") then readDirRec "${dir}/${file}" else type)
           (builtins.readDir dir);
-        getDirs = dir: pkgs.lib.attrsets.mapAttrsToList (k: v: k) (getDir dir);
+        getFirstChar = str: strings.head (strings.stringToCharacters str);
+        getDirs = dir:
+          filter (el: el != null) (pkgs.lib.attrsets.mapAttrsToList (file: type:
+            if (type == "directory" && getFirstChar file != ".") then
+              file
+            else
+              null) (builtins.readDir dir));
         files = dir:
           collect isString
           (mapAttrsRecursive (path: type: concatStringsSep "/" path)
-            (getDir dir));
+            (readDirRec dir));
         packageFiles = dir:
           map (file: ./. + "/${file}")
           (filter (file: (baseNameOf file) == "package.nix") (files dir));
