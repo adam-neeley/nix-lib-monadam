198006d48aa5ce268863926fb28928478bf86359
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-18 09:22:43 -0700

    change outputs.lib to function

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
   description = "monadam's Nix library";
 
-  inputs = { nixpkgs.url = "nixpkgs/nixos-24.05"; };
-
-  outputs = { self, nixpkgs }: { lib = import ./lib (import nixpkgs { }); };
+  outputs = { }: { lib = import ./lib; };
 }
